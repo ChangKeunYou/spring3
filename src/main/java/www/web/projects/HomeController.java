368cdd3bf6313a9a66097e3f5d6be647fdfbd167
yck <youzang7@naver.com>
2013-06-10 17:30:35 +0900

    dfsdf

--- src/main/java/www/web/projects/HomeController.java
+++ src/main/java/www/web/projects/HomeController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,8 +28,8 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	
-	 @Autowired
-	 private JavaMailSender mailSender;
+	// @Autowired
+	 //private JavaMailSender mailSender;
     
 	
 	
@@ -53,7 +53,7 @@ public class HomeController {
 		System.out.println("?????dd??=" + SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME);
 		
 		
-		logger.info("mailSender DI=>" + mailSender.getClass().getName() + " : " + mailSender.getClass().hashCode());
+		//logger.info("mailSender DI=>" + mailSender.getClass().getName() + " : " + mailSender.getClass().hashCode());
 		System.out.println("Test Jrebel....222...");
 		//????
 		model.addAttribute("serverTime", formattedDate );
